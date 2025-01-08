5556d6c1734051536f8ae2cdefee8b98fe64ef92
shbhuiyan <shbhuiyan997@gmail.com>
2025-01-08 23:02:15 +0600

    added header or nav

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,33 +1,33 @@
-// import Link from "next/link";
-// import { Button } from "../ui/button";
-// import Nav from "../Nav/Nav";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import Nav from "../Nav/Nav";
 
 
-// const Header = () => {
-//     return (
-//         <header className="py-8 xl:py-12 text-white">
-//             <div className="container mx-auto flex justify-between items-center">
-//                 {/* Logo */}
-//                 <Link href="/">
-//                     <h1 className="text-4xl font-semibold">Sakhawat<span className="text-accent">.</span></h1>
-//                 </Link>
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">Sakhawat<span className="text-accent">.</span></h1>
+                </Link>
 
-//                 {/* nav for Desktop */}
-//                 <div className="hidden xl:flex items-center gap-8">
-//                     <Nav />
-//                     <Link href="/pages/contact">
-//                         <Button>Hire Me</Button>
-//                     </Link>
-//                 </div>
+                {/* nav for Desktop */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/pages/contact">
+                        <Button>Hire Me</Button>
+                    </Link>
+                </div>
 
-//                 {/* Mobile Nav */}
-//                 <div className="xl:hidden">
-//                     mobile nav
-//                 </div>
+                {/* Mobile Nav */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
 
-//             </div>
-//         </header>
-//     );
-// };
+            </div>
+        </header>
+    );
+};
 
-// export default Header;
+export default Header;
