89bff9db35813b31c13d570ff0414d9109b4dbe8
shbhuiyan <shbhuiyan997@gmail.com>
2025-01-08 23:03:11 +0600

    added stair transition effect

--- components/Stairs/Stairs.jsx
+++ components/Stairs/Stairs.jsx
@@ -1,46 +1,46 @@
-// import { animate, motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 
-// // variants
-// const stairAnimation = {
-//   initial: {
-//     top: "0%",
-//   },
-//   animate: {
-//     top: "100%",
-//   },
-//   exit: {
-//     top: ["100%", "0%"],
-//   },
-// };
+// variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
 
-// // calculate the reverse index for staggered delay
-// const reverseIndex = (i) => {
-//   const totalSteps = 6;
-//   return totalSteps - i - 1;
-// };
+// calculate the reverse index for staggered delay
+const reverseIndex = (i) => {
+  const totalSteps = 6;
+  return totalSteps - i - 1;
+};
 
-// const Stairs = () => {
-//   return (
-//     <>
-//       {[...Array(6)].map((_, i) => {
-//         return (
-//           <motion.div
-//             key={i}
-//             variants={stairAnimation}
-//             initial="initial"
-//             animate="animate"
-//             exit="exit"
-//             transition={{
-//               duration: 0.5,
-//               ease: "easeInOut",
-//               delay: reverseIndex(i) * 0.1,
-//             }}
-//             className="h-full w-full bg-white relative"
-//           />
-//         );
-//       })}
-//     </>
-//   );
-// };
+const Stairs = () => {
+  return (
+    <>
+      {[...Array(6)].map((_, i) => {
+        return (
+          <motion.div
+            key={i}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+              delay: reverseIndex(i) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
+};
 
-// export default Stairs;
+export default Stairs;
